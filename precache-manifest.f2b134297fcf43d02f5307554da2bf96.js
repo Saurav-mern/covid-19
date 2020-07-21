4a5483a157490f69c2f9d01bb4d15fb25ae0dcf4
Saurav <sauravsingh1@outlook.in>
2020-07-22 00:17:32 +0530

    Updates

---
+++ precache-manifest.f2b134297fcf43d02f5307554da2bf96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b82f3778f197106bda10befd956c33",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "99e73097e79205b264f7",
+    "url": "/covid-19/static/css/main.cdda5b7c.chunk.css"
+  },
+  {
+    "revision": "e813c68df4048c15da73",
+    "url": "/covid-19/static/js/2.2116f2d6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.2116f2d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e73097e79205b264f7",
+    "url": "/covid-19/static/js/main.f7eebbd9.chunk.js"
+  },
+  {
+    "revision": "ec8bf7636a355fae0637",
+    "url": "/covid-19/static/js/runtime-main.b543294c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
